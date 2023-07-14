e856b57869f0a501f8f6455b4a54acbaee346ace
Geraldo Munhika <geraldosamuel916@gmail.com>
2023-07-14 20:46:34 +0100

    Streaming data to browser

--- client.js
+++ client.js
@@ -1,39 +1,61 @@
-const response = await fetch('http://127.0.0.1:8000');
-const reader = response.body
-let i = 0
-reader.pipeThrough(new TextDecoderStream())  
-        .pipeThrough(
-            new TransformStream({
-                transform(chunk,controller){
-                    
-                    for(const item of chunk.split('\n')){
+
+async function fetchData(){
+    const response = await fetch('http://127.0.0.1:8000');
+    const reader = response.body
+    let i = 0
+
+    reader.pipeThrough(new TextDecoderStream())  
+            .pipeThrough(
+                new TransformStream({
+                    transform(chunk,controller){
+                        
+                        for(const item of chunk.split('\n')){
                         try{
-                            if(!item.length)continue;
-                            i = i +1
-                            controller.enqueue(JSON.parse(item));
-                        }catch(err){
-                            console.log(err);
+                                if(!item.length)continue;
+                                i = i +1
+                                const dataParsed = JSON.parse(item)
+                                const data = {
+                                    ...dataParsed,
+                                    date: new Date(dataParsed.date).toLocaleDateString()
+                                }
+
+                                controller.enqueue(data);
+                            }catch(err){
+                                //console.log(err);
+                            }
                         }
                     }
-                }
-            })
-        ).pipeTo(
-            new WritableStream({
-                write(data){
-                    console.log(data)
-                    console.log(i)
-                }
-            })
-        )
+                }))
+                    .pipeTo(
+                    new WritableStream({
+                        write(data){
+                            attachElement(data)
+                        }
+                    }))
+}
 
-/*
-readableStream.pipe(
-    Transform({
-        objectMode:true,
-        transform(chunk,enc,cb){
-            const item = JSONStream.parse(chunk);
-            item.at = `${new Date(item.at).toLocaleDateString()}`
-            cb(null,JSONStream.stringify(item).)
-        }
-        
-})).pipe(process.stdout)*/
+
+
+
+function attachElement({user_id,name,date}){
+    const listEl = document.querySelector('#data-list');
+
+    const node =  `
+    <li>
+        <div>
+            <span>Id:</span><h5>${user_id}</h5> 
+        </div>
+        <div>
+            <span>Name:</span><h3>${name}</h3>
+        </div>
+        <div>
+            <span>Created at:<h3>${date}</h3></span>
+        </div>
+    </li>
+    `
+    listEl.innerHTML += node;
+}        
+
+(async()=>{
+    await fetchData()
+})()
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import http from 'node:http';
 import db from './sqlLite.js';
 import { Readable, Transform, Writable } from 'node:stream';
+import { setTimeout } from 'node:timers/promises';
 const PORT = process.env.PORT || 8000;
 const HOST = '127.0.0.1';
 
 function handler(req,res){
+
+    //cors policy headers
+    const headers = {
+        'Access-Control-Allow-Origin':'*',
+    }
+
+    //send the response header
+    res.writeHead(200,headers)
+
     // Execute a query and stream the results
     const writeStream = new Readable({
         read(){
@@ -24,13 +34,18 @@ function handler(req,res){
     })
     writeStream.pipe(
         Transform({
-            transform(chunk,enc,cb){
+            async transform(chunk,enc,cb){
                 const data = JSON.parse(chunk);
                 const dataParsed = {
                     user_id: data.id,
                     name:data.name,
                     date: data.at
                 }
+
+                //This allows the event loop to process other tasks and ensures 
+                //that the transformed data is correctly sent through the stream 
+                await setTimeout(0)
+                //This ensures that each transformed chunk is separated by a newline.
                 cb(null,JSON.stringify(dataParsed).concat('\n'));
             }
         })
